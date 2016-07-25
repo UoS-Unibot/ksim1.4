4d250a3e9b8a7e913d099f152fd8db80f2fe8dd9
mmg20 <miguelgarvie@gmail.com>
2016-07-25 13:38:19 +0100

    To simulate bug in K-Junior IR with turret

---
+++ src/org/evors/rs/kjunior/BuggyIRSimulatedKJunior.java
@@ -0,0 +1,40 @@
+package org.evors.rs.kjunior;
+
+import org.evors.rs.sim.core.SimulationWorld;
+
+public class BuggyIRSimulatedKJunior extends SimulatedKJunior
+{
+	double d_th = 3583;
+	double d_0 = 3450;
+	double delta = 2935;
+	
+	int FRONT_IR_IX = 2;
+	
+	public BuggyIRSimulatedKJunior(SimulationWorld world, double timeStepLength )
+	{
+		super( world, timeStepLength );
+	}
+	
+	public double[] getInput()
+	{
+		double[] rv = super.getInput();
+		
+		double front = rv[ FRONT_IR_IX ];
+		
+		if( front > d_th )
+		{
+			front -= delta;
+		}else if( front < d_0 )
+		{
+			front = 0;
+		}else
+		{
+			front = ( front - delta ) * ( d_0 - front ) / ( d_0 - d_th );
+		}
+		
+		rv[ FRONT_IR_IX ] = front;
+		
+		return rv;
+	}
+	
+}
