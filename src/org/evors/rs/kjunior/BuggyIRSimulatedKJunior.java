3a2b62197b39a357bef9c6e2466951a3923afedb
mmg20 <miguelgarvie@gmail.com>
2016-07-28 14:36:14 +0100

    Better toString in KJ robot bug simulator

--- src/org/evors/rs/kjunior/BuggyIRSimulatedKJunior.java
+++ src/org/evors/rs/kjunior/BuggyIRSimulatedKJunior.java
@@ -4,11 +4,14 @@ import org.evors.rs.sim.core.SimulationWorld;
 
 public class BuggyIRSimulatedKJunior extends SimulatedKJunior
 {
+	int bugTypeIx = 0;
+	
 	double d_th = 3583;
 	double d_0 = 3450;
 	double delta = 2935;
 	
-	int FRONT_IR_IX = 2;
+	static final int FRONT_IR_IX = 2;
+	static final String[] bugTypeStr = { "Off", "Weak" };
 	
 	public BuggyIRSimulatedKJunior(SimulationWorld world, double timeStepLength )
 	{
@@ -21,19 +24,32 @@ public class BuggyIRSimulatedKJunior extends SimulatedKJunior
 		
 		double front = rv[ FRONT_IR_IX ];
 		
-		if( front > d_th )
-		{
-			front -= delta;
-		}else if( front < d_0 )
+		if( bugTypeIx == 0 )
 		{
 			front = 0;
 		}else
 		{
-			front = ( front - delta ) * ( d_0 - front ) / ( d_0 - d_th );
+			if( front > d_th )
+			{
+				front -= delta;
+			}else if( front < d_0 )
+			{
+				front = 0;
+			}else
+			{
+				front = ( front - delta ) * ( d_0 - front ) / ( d_0 - d_th );
+			}
 		}
 		
-		rv[ FRONT_IR_IX ] = 0; // *** front;
+		rv[ FRONT_IR_IX ] = front;
 		
+		return rv;
+	}
+
+	public String toString()
+	{
+		String rv = "BuggyIRSimulatedKJunior with bug type = " + bugTypeStr[ bugTypeIx ];
+		rv += "\r\n\t\t\tinner robot: " + super.toString();
 		return rv;
 	}
 	
